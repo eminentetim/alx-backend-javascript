29c0c53b562696c455bff55c924b446b65ec502a
eminentetim <etimeminent@gmail.com>
2024-04-22 21:12:53 +0100

    Creating task

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,14 +1,15 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
+    var task = 'I prefer const when I can.';
     return task;
-}
-
-export function taskLast() {
-   return 'is okay';
-}
-
-export function taskNext() {
-   let combination = 'but sometimes lat';
-   combination = getLast();
-   return combination;
-}
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
