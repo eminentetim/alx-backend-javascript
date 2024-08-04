f95a6a1cbd966d7c465d3de74724ff3f1f448407
eminentetim <etimeminent@gmail.com>
2024-08-04 23:09:59 +0100

    updating

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,30 +1,50 @@
+import Currency from './3-currency';
+
 export default class Pricing {
-    constructor(amount, currency) {
-      this._amount = amount;
-      this._currency = currency;
-    }
-  
-    get amount() {
-      return this._amount;
-    }
-  
-    set amount(a) {
-      this._amount = a;
-    }
-  
-    get currency() {
-      return this._currency;
-    }
-  
-    set currency(c) {
-      this._currency = c;
-    }
-  
-    displayFullPrice() {
-      return `${this.amount} ${this.currency._name} (${this.currency._code})`;
-    }
-  
-    static convertPrice(amount, conversionRate) {
-      return (amount * conversionRate);
-    }
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
   }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  /**
+   * @returns {Currency}
+   */
+  get currency() {
+    return this._currency;
+  }
+
+  /**
+   * @param {Currency} value
+   */
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
